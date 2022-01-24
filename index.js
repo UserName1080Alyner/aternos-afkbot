d9de34cdabc57ae927b7b30d2b54558edabd0780
UserName1080Alyner <76223883+UserName1080Alyner@users.noreply.github.com>
2022-01-24 14:53:11 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 const mineflayer = require('mineflayer')
 
-bot3=mineflayer.createBot({
+bot=mineflayer.createBot({
 	host: 'jogar.survivalcomplecxo2.ga',
 	port: '22875',
 	username: 'SrStevon3',
 	version: '1.13'
 })
 
-bot4=mineflayer.createBot({
-	host: 'jogar.survivalcomplecxo2.ga',
-	port: '22875',
-	username: 'SrStevon4',
-	version: '1.13'
-})
-
-bot3.on('spawn',function() {
+bot.on('spawn',function() {
     bot.chat('/login 123123')
-});
-
-
-bot4.on('spawn',function() {
-    
 });
