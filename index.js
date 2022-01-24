3db641673920941218897be693f3f4b85d655f11
UserName1080Alyner <76223883+UserName1080Alyner@users.noreply.github.com>
2022-01-24 14:34:53 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const mineflayer = require('mineflayer')
 
-
-bot=mineflayer.createBot({
+bot1=mineflayer.createBot({
 	host: 'jogar.survivalcomplecxo2.ga',
 	port: '22875',
 	username: 'SrStevon',
 	version: '1.13'
 })
 
+bot2=mineflayer.createBot({
+	host: 'jogar.survivalcomplecxo2.ga',
+	port: '22875',
+	username: 'SrStevon2',
+	version: '1.13'
+})
+
+bot3=mineflayer.createBot({
+	host: 'jogar.survivalcomplecxo2.ga',
+	port: '22875',
+	username: 'SrStevon3',
+	version: '1.13'
+})
+
+bot4=mineflayer.createBot({
+	host: 'jogar.survivalcomplecxo2.ga',
+	port: '22875',
+	username: 'SrStevon4',
+	version: '1.13'
+})
+
+bot5=mineflayer.createBot({
+	host: 'jogar.survivalcomplecxo2.ga',
+	port: '22875',
+	username: 'SrStevon5',
+	version: '1.13'
+})
